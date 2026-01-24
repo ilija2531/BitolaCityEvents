8c8447d2dd26332e3f62fb0ad3947371f91586a8
ilija2531 <bogdanovskiilija18@gmail.com>
2026-01-24 19:58:39 +0100

    initial setup

---
+++ bitolacityevents.frontend/app/events/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Events() {
+  return (
+    <main>
+      <h1>Upcoming Events</h1>
+      <ul>
+        <li>Event 1</li>
+        <li>Event 2</li>
+        <li>Event 3</li>
+      </ul>
+    </main>
+  )
+}
---
+++ bitolacityevents.frontend/app/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Welcome to Bitola City Events</h1>
+    </main>
+  )
+}
